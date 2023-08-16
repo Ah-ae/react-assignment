319ac18cf7dc04eed46af10b88943daef8cb3b0c
Ahae KIM <kimaae@gmail.com>
2023-08-16 11:20:00 +0900

    clear #8

--- src/homework/08/index.tsx
+++ src/homework/08/index.tsx
@@ -1,30 +1,57 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import styled from "styled-components";
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import styled from 'styled-components';
 
 interface IRobotStatus {
-  //
+  position: number;
+  direction: 'right' | 'left';
 }
 
 class Robot {
-  //
+  // position: number;
+  // direction: 'right' | 'left';
+  position: IRobotStatus['position'];
+  direction: IRobotStatus['direction'];
+
+  constructor() {
+    this.position = 0;
+    this.direction = 'right';
+  }
+
+  go() {
+    if (this.direction === 'right') this.position++;
+    else this.position--;
+  }
+
+  turn() {
+    if (this.direction === 'right') this.direction = 'left';
+    else this.direction = 'right';
+  }
+
+  getStatus() {
+    return {
+      position: this.position,
+      direction: this.direction,
+    };
+  }
 }
 const myRobot = new Robot();
 
 const Homework = () => {
-  // const [history, setHistory] = useState<IRobotStatus[]>([myRobot.getStatus()]);
-  // const handleGoBtn = () => {
-  //   myRobot.go();
-  //   setHistory((prev) => [...prev, myRobot.getStatus()]);
-  // };
-  // const handleTurnBtn = () => {
-  //   myRobot.turn();
-  //   setHistory((prev) => [...prev, myRobot.getStatus()]);
-  // };
+  const [history, setHistory] = useState<IRobotStatus[]>([myRobot.getStatus()]);
+  const handleGoBtn = () => {
+    myRobot.go();
+    setHistory((prev) => [...prev, myRobot.getStatus()]);
+  };
+  const handleTurnBtn = () => {
+    myRobot.turn();
+    setHistory((prev) => [...prev, myRobot.getStatus()]);
+  };
+  console.log(history);
   return (
     <>
-      {/* <Btn onClick={handleGoBtn}>Go</Btn>
+      <Btn onClick={handleGoBtn}>Go</Btn>
       <Btn onClick={handleTurnBtn}>Turn</Btn>
       <Btn onClick={() => setHistory([])}>Clear</Btn>
       <ul>
@@ -33,7 +60,7 @@ const Homework = () => {
             {el.direction} {el.position}
           </li>
         ))}
-      </ul> */}
+      </ul>
     </>
   );
 };
